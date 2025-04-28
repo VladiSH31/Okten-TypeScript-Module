031b8828783246a542aa2560ad49d4558946a4a0
VladiSH31 <vladislav.sheremet@ukr.net>
2025-04-28 12:57:08 +0300

    -made cw 2 1-2 DOM and Async

---
+++ Lesson 2/CW/cw2.js
@@ -0,0 +1,10 @@
+"use strict";
+let elementsByClassName = document.getElementsByClassName('target');
+console.log(elementsByClassName);
+let elementsByTagName = document.getElementsByTagName('div');
+let forms = document.forms;
+let f1 = forms[0];
+let f1ElementUsername = f1['username'];
+let f1ElementPassword = f1['password'];
+console.log(f1ElementUsername);
+console.log(f1ElementPassword);
---
+++ Lesson 2/CW/cw2.ts
@@ -0,0 +1,34 @@
+let elementsByClassName: HTMLCollectionOf<Element> = document.getElementsByClassName('target');
+
+console.log(elementsByClassName);
+
+let elementsByTagName: HTMLCollectionOf<HTMLDivElement> = document.getElementsByTagName('div');
+
+let forms: HTMLCollectionOf<HTMLFormElement> = document.forms;
+let f1: HTMLFormElement = forms[0];
+
+let f1ElementUsername: HTMLInputElement = f1['username'];
+let f1ElementPassword = f1['password'];
+
+console.log(f1ElementUsername);
+console.log(f1ElementPassword);
+
+// TS. Async типізація
+
+interface IUser {
+    id: number;
+    name: string;
+}
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(value => value.json())
+    .then((users: IUser[]) => {
+        console.log(users[0].name)
+    })
+
+
+async function foobar(): Promise<IUser[]> {
+    let promise: IUser[] = await fetch('https://jsonplaceholder.typicode.com/users')
+        .then(value => value.json())
+    return promise;
+}
