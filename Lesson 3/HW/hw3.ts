bad5a93eb6131cd1dd553347bcf012210d377953
VladiSH31 <vladislav.sheremet@ukr.net>
2025-04-30 09:04:17 +0300

    -made hw 3 a

--- Lesson 3/HW/hw3.ts
+++ Lesson 3/HW/hw3.ts
@@ -1,3 +1,34 @@
+// TS. ДЗ 3
+// Створити функцію, яка робить запити на апі, та віддає результат свого запиту. Тип відповіді визначається дженеріком функції.
+//
+// Сигнатур функції під час виклику foobar<SomeType>(‘/url)
+//
+// SomeType – ваш тип відповіді
+//
+// url – ваш ендпоінт, з якого чекаємо відповідь
+
+
+//interface IUSerDummy {
+//     id: number,
+//     firstName: string,
+//     lastName: string,
+//     maidenName: string,
+// }
+//
+// interface dummyUser {
+//     users: IUSerDummy[];
+// }
+//
+// function foobar2<T>(url: string) {
+//     fetch(url)
+//         .then(res => res.json())
+//         .then((value: T) => console.log(value));
+// }
+//
+//
+//
+// foobar2<dummyUser>('https://dummyjson.com/users')
+
 interface IUSerDummy {
     id: number;
     firstName: string;
